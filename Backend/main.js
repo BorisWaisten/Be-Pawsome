580865445b1249bda250a89c1a975dcba7a6834d
BorisWaisten <boriswaisten@gmail.com>
2023-09-26 10:39:30 -0300

    instalacion de paquetes

---
+++ Backend/main.js
@@ -0,0 +1,18 @@
+import express  from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+// facilidad de usar json
+app.use(bodyParser.json());// para datos JSON
+app.use(bodyParser.urlencoded({extended: true}));//para datos de formularios
+
+//regula las solicitudes de un dominio diferente al servidor en el que se encuentra la app
+app.use(cors());
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+})
