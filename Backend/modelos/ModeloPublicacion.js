2c901d4098294cdd355c200e72fd2a76baa116c6
BorisWaisten <boriswaisten@gmail.com>
2023-09-29 12:12:32 -0300

    Avance: token en register y login

--- Backend/modelos/ModeloPublicacion.js
+++ Backend/modelos/ModeloPublicacion.js
@@ -5,15 +5,30 @@ const ESTADOPUBLICACION = {
   };
   
   class Publicacion {
-    constructor(fechaCreacion, fechaCaducidad, estadoPublicacion, titulo, idUsuario, idAnimal, idPublicacion) {
-      this.fechaCreacion = fechaCreacion;
-      this.fechaCaducidad = fechaCaducidad;
-      this.estadoPublicacion = estadoPublicacion;
+    constructor(titulo, idUsuario, idAnimal, idPublicacion) {
+      this.fechaCreacion = new Date().getDate() + "/" + new Date().getMonth() + "/" + new Date().getFullYear();
+      this.fechaCaducidad = this.calcularFechaCaducidad();
+      this.estadoPublicacion = ESTADOPUBLICACION.ACTIVA;
       this.titulo = titulo;
       this.idUsuario = idUsuario;
       this.idAnimal = idAnimal;
       this.idPublicacion = idPublicacion;
     }
+    
+    calcularFechaCaducidad() {
+      const fechaCreacion = new Date(this.fechaCreacion);
+      fechaCreacion.setDate(fechaCreacion.getDate() + 1);// Agrega 90 días a la fecha de creación
+      return fechaCreacion;
+    }
+
+    getEstadoCaducidad(){
+      const fechaActual=new Date()
+      if(fechaActual>=this.fechaCaducidad){
+        estado=ESTADOPUBLICACION.CADUCADA
+      }
+      return estado
+    }
+
   }
   
   module.exports = { Publicacion, ESTADOPUBLICACION };
