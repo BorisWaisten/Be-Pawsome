628068ceb02dd9e8d274f1cdc0e31de76d77865b
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-09-30 16:18:30 -0300

    Se modifica Clase Animal campo adoptante

--- Backend/modelos/modeloAnimal.js
+++ Backend/modelos/modeloAnimal.js
@@ -1,5 +1,5 @@
  class Animal {
-    constructor(nombre, fotos, edad, tipoAnimal, descripcion, sexo, pesoEnKg, ubicacion, oferente, adoptante, historiaClinica) {
+    constructor(nombre, fotos, edad, tipoAnimal, descripcion, sexo, pesoEnKg, ubicacion, oferente,historiaClinica) {
       this.nombre = nombre;
       this.fotos = fotos;
       this.edad = edad;
@@ -9,7 +9,7 @@
       this.pesoEnKg = pesoEnKg;
       this.ubicacion = ubicacion;
       this.oferente = oferente;
-      this.adoptante = adoptante;
+      this.adoptante = null;
       this.historiaClinica = historiaClinica;
     }
   }
