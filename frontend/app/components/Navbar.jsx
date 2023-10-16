0479886ab6e1a3946f21e3261c60de901fb6f5b3
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-10-16 13:40:47 -0300

    Auto formato

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import Link from "next/link"
-import Image from "next/image"
-import Logo from "public/logoBePawsome.png"
-import { REACT_LOADABLE_MANIFEST } from "next/dist/shared/lib/constants"
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "public/logoBePawsome.png";
+import { REACT_LOADABLE_MANIFEST } from "next/dist/shared/lib/constants";
+
 export default function NavBar() {
   return (
     <nav className="flex items-center justify-between py-4 px-6">
       <div className="flex items-center space-x-4">
         <Image
-          src={Logo} 
-          alt='Galajo Logo'
+          src={Logo}
+          alt="Galajo Logo"
           width={70}
           height={70}
           quality={100}
@@ -18,14 +19,14 @@ export default function NavBar() {
         <Link href="/aboutUs">About Us</Link>
         <Link href="/register">Registrarse</Link>
         <Link href="/publicacion">Crear Publicacion</Link>
+
         {/*<Link href="/publicacion">Publicacion</Link>*/}
         {/*<Link href="/usuario/register">Register</Link>*/}
-        {/*<Link className='' href="/register">Registrarse</Link>*/}      
+        {/*<Link className='' href="/register">Registrarse</Link>*/}
+        {/*isLoggedIn?(<Link href="/usuario">Ajustes de Usuario</Link>) : <p> No hay nada</p>*/}
         <Link href="/usuario">Ajustes de Usuario</Link>
-        
       </div>
       <Link href="/login">Login</Link>
     </nav>
-  )
+  );
 }
-
