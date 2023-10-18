1f4f0a22eca10ffd82d651a64d0a50ae3a79c2a2
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-10-18 17:08:42 -0300

    instalacion paquete cloudinary para react

--- frontend/app/publicacion/CreatePublicacion.jsx
+++ frontend/app/publicacion/CreatePublicacion.jsx
@@ -1,5 +1,9 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import {Cloudinary} from "@cloudinary/url-gen";
+
+const cld = new Cloudinary({cloud: {cloudName: 'bepawsome'}});
+
 
 const CreatePublicacion = ({ handleSubmit, formData, updateFormData }) => {
   const [localFormData, setFormData] = useState({
