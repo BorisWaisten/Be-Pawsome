ce72d8107f26b8dddfffe5231a3f18f594f63d6e
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-10-18 22:24:40 -0300

    footer creado

---
+++ frontend/app/components/layout/Footer.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <div id="seccion_footer" class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+      <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
+        <p>© 2023 Copyright: GALAJO ¿Qué puedo hacer en caso de maltrato o crueldad con animales?</p>
+        <a
+          class="text-neutral-800 dark:text-neutral-400"
+          href="https://www.argentina.gob.ar/normativa/nacional/ley-14346-153011/texto"
+        >
+          <p>Ir a Proteccion de los animales </p>
+        </a>
+      
+      </div>
+    </div>
+  );
+}
--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,25 +1,24 @@
-import NavBar from './components/layout/Navbar'
-import './globals.css'
-import { Roboto_Slab } from 'next/font/google'
-
+import NavBar from "./components/layout/Navbar";
+import "./globals.css";
+import { Roboto_Slab } from "next/font/google";
+import Footer from "./components/layout/Footer";
 // Aca Van a ir todos los componentes que voy a ir importando
 
-
-
-const roboto = Roboto_Slab({ subsets: ['latin'] })
-
+const roboto = Roboto_Slab({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'BePawsome',
-  description: 'Adopta una vida',
-}
+  title: "BePawsome",
+  description: "Adopta una vida",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
         <NavBar />
-        {children}</body>
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
