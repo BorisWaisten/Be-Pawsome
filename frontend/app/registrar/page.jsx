6a1741591b61a5010d07cc5963402ae9cc3cb5e0
Julian <julianstravitz@gmail.com>
2023-10-19 17:02:23 -0300

    estilando publicaciones en perfil

--- frontend/app/registrar/page.jsx
+++ frontend/app/registrar/page.jsx
@@ -6,7 +6,6 @@ var error = null;
 export default function RegistrarPage() {
   return (
     <main>
-      <h1>Ud. Esta en Registrarse</h1>
       <FormRegister/>
       {error && <h5 className="text-danger">{error}</h5>}
     </main>
--- frontend/app/usuario/perfil/DatosPerfil.jsx
+++ frontend/app/usuario/perfil/DatosPerfil.jsx
@@ -158,118 +158,138 @@ export default function Usuario() {
           style={{ width: "150px", height: "150px" }}
         />
       </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.nombre}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.apellido}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.celular}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.localidad}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.provincia}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.nacionalidad}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={usuario.codigoPostal}
-          disabled
-          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
-        />
-        <button
-          onClick={handleEditarClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
-        >
-          Editar
-        </button>
+      <div className="items-center flex flex-col">
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.nombre}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.apellido}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.celular}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.localidad}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.provincia}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.nacionalidad}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            value={usuario.codigoPostal}
+            disabled
+            className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+          />
+          <button
+            onClick={handleEditarClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+          >
+            Editar
+          </button>
+        </div>
       </div>
 
       {/* Lista de publicaciones del usuario */}
-      <h1 className="">Publicaciones del usuario</h1>
-      <ul>
-        {Array.isArray(publicaciones) &&
-          publicaciones.map((publicacion) => (
-            <li key={publicacion._id}>
+      <h1 className="mt-3">Publicaciones del usuario</h1>
+      <ul className="divide-y divide-violet-200">
+  {Array.isArray(publicaciones) &&
+    publicaciones.map((publicacion) => (
+      <li key={publicacion._id} className="my-4">
+        <div className="flex items-center justify-between space-x-5 border-1 border-gray-200 py-4 px-2">
+          <div className="rounded-lg w-24 h-24 overflow-hidden">
+            <img
+              src={publicacion.animal.fotos[0]}
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <div className="flex-1">
+            <div className="text-lg font-medium text-violet-900 truncate">
               {publicacion.titulo}
-              <button onClick={() => confirmarEliminar(publicacion._id)}>
-                Eliminar
-              </button>
-            </li>
-          ))}
-      </ul>
+            </div>
+            <div className="text-sm text-black-500">
+              {publicacion.animal.descripcion}
+            </div>
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => confirmarEliminar(publicacion._id)}
+          >
+            Eliminar
+          </button>
+        </div>
+      </li>
+    ))}
+</ul>
 
       {/* Modal para editar los datos del usuario */}
       {modalVisible && (
