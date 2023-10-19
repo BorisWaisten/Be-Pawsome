f0ba2e9e5c919a2168bd9097067c7f0cda944264
Julian <julianstravitz@gmail.com>
2023-10-18 22:07:53 -0300

    estilando perfil

--- frontend/app/components/Publicaciones/PublicacionesList.jsx
+++ frontend/app/components/Publicaciones/PublicacionesList.jsx
@@ -6,6 +6,7 @@ export default async function PublicacionesList() {
   
   var publicaciones = await getPublicaciones();
   const { usuario } = await obtenerUsuarioLogeado();
+
   if (publicaciones && usuario) {
     const publicacionesAMostrar = [];
     publicaciones.forEach(p => {
@@ -19,7 +20,7 @@ export default async function PublicacionesList() {
 
   return (
         <>
-          <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+          <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-10">
            {publicaciones.map(publicacion => (
              <li key={publicacion._id} className="w-full h-full">
               <CartaPublicacion publicacion={publicacion} />
--- frontend/app/usuario/perfil/DatosPerfil.jsx
+++ frontend/app/usuario/perfil/DatosPerfil.jsx
@@ -1,6 +1,10 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import { obtenerUsuarioLogeado, editarUsuario, obtenerPublicacionesDelUsuario } from '@/app/persistencia/peticiones';
+import React, { useEffect, useState } from "react";
+import {
+  obtenerUsuarioLogeado,
+  editarUsuario,
+  obtenerPublicacionesDelUsuario,
+} from "@/app/persistencia/peticiones";
 
 export default function Usuario() {
   //const router = useRouter();
@@ -10,17 +14,15 @@ export default function Usuario() {
   const [modalVisible, setModalVisible] = useState(false); // Estado para mostrar/ocultar el modal
   const [publicaciones, setPublicaciones] = useState([]);
 
-
   const [nuevosDatos, setNuevosDatos] = useState({
     // imagenPerfil: "https://img2.freepng.es/20180331/khw/kisspng-computer-icons-user-clip-art-user-5abf13d4b67e20.4808850915224718927475.jpg",
-    nombre: '',
-    apellido: '',
-    celular: '',
-    localidad: '',
-    provincia: '',
-    nacionalidad: '',
-    codigoPostal: '',
-    
+    nombre: "",
+    apellido: "",
+    celular: "",
+    localidad: "",
+    provincia: "",
+    nacionalidad: "",
+    codigoPostal: "",
   });
 
   const cargarUsuario = async () => {
@@ -29,10 +31,12 @@ export default function Usuario() {
       if (usuario) {
         setUsuario(usuario);
         // Obtener las publicaciones del usuario
-        const { publicaciones } = await obtenerPublicacionesDelUsuario(usuario._id);
+        const { publicaciones } = await obtenerPublicacionesDelUsuario(
+          usuario._id
+        );
         setPublicaciones(publicaciones);
         // Inicializa los nuevos datos con los valores del usuario
-        
+
         setNuevosDatos(usuario);
       } else {
         setError(error);
@@ -43,13 +47,10 @@ export default function Usuario() {
       setLoading(false);
     }
   };
-  
+
   useEffect(() => {
     cargarUsuario();
   }, []);
-
- 
-
 
   const handleEditarClick = () => {
     setModalVisible(true); // Mostrar el modal al hacer clic en "Editar"
@@ -70,16 +71,23 @@ export default function Usuario() {
     e.preventDefault();
     try {
       // Hacer una solicitud PUT para actualizar los datos del usuario en el backend
-      
-      const { _id,mail,password,imagenPerfil,esAdmin,casita, ...datosNecesarios } = nuevosDatos;
+
+      const {
+        _id,
+        mail,
+        password,
+        imagenPerfil,
+        esAdmin,
+        casita,
+        ...datosNecesarios
+      } = nuevosDatos;
       console.log(datosNecesarios);
       const usuarioEditado = await editarUsuario(usuario._id, datosNecesarios);
-      
+
       setUsuario(usuarioEditado);
 
       setModalVisible(false); // Cerrar el modal después de guardar los cambios
       //router.push('/usuario');
-
     } catch (error) {
       console.error(error);
       // Manejar errores, mostrar un mensaje al usuario, etc.
@@ -89,14 +97,16 @@ export default function Usuario() {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     // Actualizar el estado de los nuevos datos cuando el usuario edita los campos del formulario
-    setNuevosDatos(prevState => ({
+    setNuevosDatos((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const confirmarEliminar = (publicacionId) => {
-    const confirmacion = window.confirm("¿Estás seguro de que deseas eliminar esta publicación?");
+    const confirmacion = window.confirm(
+      "¿Estás seguro de que deseas eliminar esta publicación?"
+    );
     if (confirmacion) {
       // Llamar a la función para eliminar la publicación
       eliminarPublicacion(publicacionId);
@@ -107,10 +117,13 @@ export default function Usuario() {
     try {
       // Hacer una solicitud DELETE para eliminar la publicación en el backend
       // Implementa esta función usando fetch o axios
-      await fetch(`http://localhost:5000/publicacion/eliminar/${publicacionId}`, {
-        method: "DELETE",
-      });
-  
+      await fetch(
+        `http://localhost:5000/publicacion/eliminar/${publicacionId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
       // Actualizar la lista de publicaciones después de eliminar
       const nuevasPublicaciones = publicaciones.filter(
         (publicacion) => publicacion._id !== publicacionId
@@ -137,25 +150,123 @@ export default function Usuario() {
   // Muestra los detalles del usuario y el botón para abrir el modal de edición
   return (
     <main>
-      <h2>Usuario</h2>
-      <img src={usuario.imagenPerfil} alt="Foto de perfil" style={{ width: '150px', height: '150px' }} />
-      <p>Nombre: {usuario.nombre}</p>
-      <p>Apellido: {usuario.apellido}</p>
-      <p>Celular: {usuario.celular}</p>
-      <p>Localidad: {usuario.localidad}</p>
-      <p>Provincia: {usuario.provincia}</p>
-      <p>Nacionalidad: {usuario.nacionalidad}</p>
-      <p>Código Postal: {usuario.codigoPostal}</p>
-      <button onClick={handleEditarClick}>Editar</button>
+      <div className="flex justify-center items-center">
+        <img
+          className=" flex rounded-full m-10 justify-center items-center"
+          src={usuario.imagenPerfil}
+          alt="Foto de perfil"
+          style={{ width: "150px", height: "150px" }}
+        />
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.nombre}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.apellido}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.celular}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.localidad}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.provincia}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.nacionalidad}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
+      <div className="w-full flex">
+        <input
+          type="text"
+          value={usuario.codigoPostal}
+          disabled
+          className="w-1/2 bg-gray-200 h-8 rounded mb-4 px-4 text-2xl text-center flex-2"
+        />
+        <button
+          onClick={handleEditarClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 h-8"
+        >
+          Editar
+        </button>
+      </div>
 
       {/* Lista de publicaciones del usuario */}
-      <h3>Publicaciones del usuario:</h3>
+      <h1 className="">Publicaciones del usuario</h1>
       <ul>
         {Array.isArray(publicaciones) &&
           publicaciones.map((publicacion) => (
             <li key={publicacion._id}>
               {publicacion.titulo}
-              <button onClick={() => confirmarEliminar(publicacion._id)}>Eliminar</button>
+              <button onClick={() => confirmarEliminar(publicacion._id)}>
+                Eliminar
+              </button>
             </li>
           ))}
       </ul>
@@ -170,31 +281,66 @@ export default function Usuario() {
             </label> */}
             <label>
               Nombre:
-              <input type="text" name="nombre" value={nuevosDatos.nombre} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="nombre"
+                value={nuevosDatos.nombre}
+                onChange={handleInputChange}
+              />
             </label>
             <label>
               Apellido:
-              <input type="text" name="apellido" value={nuevosDatos.apellido} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="apellido"
+                value={nuevosDatos.apellido}
+                onChange={handleInputChange}
+              />
             </label>
             <label>
               Celular:
-              <input type="text" name="celular" value={nuevosDatos.celular} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="celular"
+                value={nuevosDatos.celular}
+                onChange={handleInputChange}
+              />
             </label>
             <label>
               Localidad:
-              <input type="text" name="localidad" value={nuevosDatos.localidad} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="localidad"
+                value={nuevosDatos.localidad}
+                onChange={handleInputChange}
+              />
             </label>
             <label>
               Provincia:
-              <input type="text" name="provincia" value={nuevosDatos.provincia} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="provincia"
+                value={nuevosDatos.provincia}
+                onChange={handleInputChange}
+              />
             </label>
             <label>
               Nacionalidad:
-              <input type="text" name="nacionalidad" value={nuevosDatos.nacionalidad} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="nacionalidad"
+                value={nuevosDatos.nacionalidad}
+                onChange={handleInputChange}
+              />
             </label>
             <label>
               Código Postal:
-              <input type="text" name="codigoPostal" value={nuevosDatos.codigoPostal} onChange={handleInputChange} />
+              <input
+                type="text"
+                name="codigoPostal"
+                value={nuevosDatos.codigoPostal}
+                onChange={handleInputChange}
+              />
             </label>
             <button type="submit">Guardar Cambios</button>
             <button onClick={() => setModalVisible(false)}>Cancelar</button>
