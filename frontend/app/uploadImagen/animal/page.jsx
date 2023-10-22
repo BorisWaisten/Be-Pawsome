95c098a67cfacfae5f600bba9a8d83012f33fe38
BorisWaisten <boriswaisten@gmail.com>
2023-10-22 12:15:01 -0300

    Se puede cargar mas de una Imagen, falta renderizar varios archivos

--- frontend/app/uploadImagen/animal/page.jsx
+++ frontend/app/uploadImagen/animal/page.jsx
@@ -7,34 +7,37 @@ function ImagenAnimal({ onImageUpload }) {
 
   const onDrop = async (acceptedFiles) => {
     try {
-      const file = acceptedFiles[0];
-      const dataImagen = new FormData();
-      dataImagen.append('file', file);
-      dataImagen.append('upload_preset', 'animales');
-
-      const response = await fetch('https://api.cloudinary.com/v1_1/bepawsome/image/upload', {
-        method: 'POST',
-        body: dataImagen,
-      });
-
-      if (!response.ok) {
-        throw new Error('Error en la carga de la imagen');
+      // Itera sobre cada archivo aceptado
+      for (const file of acceptedFiles) {
+        const dataImagen = new FormData();
+        dataImagen.append('file', file);
+        dataImagen.append('upload_preset', 'animales');
+  
+        const response = await fetch('https://api.cloudinary.com/v1_1/bepawsome/image/upload', {
+          method: 'POST',
+          body: dataImagen,
+        });
+  
+        if (!response.ok) {
+          throw new Error('Error en la carga de la imagen');
+        }
+  
+        const data = await response.json();
+        setUploadData(data);
+  
+        // Llama a la función onImageUpload con el secure_url como argumento
+        onImageUpload(data.secure_url);
       }
-
-      const data = await response.json();
-      setUploadData(data);
-
-      // Llama a la función onImageUpload con el secure_url como argumento
-      onImageUpload(data.secure_url);
     } catch (error) {
       console.error('Error en la carga de la imagen:', error);
       setError('Error en la carga de la imagen. Por favor, inténtalo de nuevo.');
     }
   };
+  
 
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
-    accept: 'image/*', // Acepta solo archivos de imagen
+    accept: 'image/*',
   });
 
   return (
@@ -43,16 +46,34 @@ function ImagenAnimal({ onImageUpload }) {
         <h1>Subir Imagen Animal</h1>
         {error && <p style={{ color: 'red' }}>{error}</p>}
 
-        {/* Use Dropzone component */}
-        <div {...getRootProps()}>
+        <div
+          {...getRootProps()}
+          style={{
+            border: '2px dashed #777',
+            borderRadius: '8px',
+            padding: '20px',
+            textAlign: 'center',
+            cursor: 'pointer',
+            backgroundColor: isDragActive ? '#eee' : '#fff',
+          }}
+        >
           <input {...getInputProps()} />
-          <p>Arrastra y suelta una imagen aquí o haz clic para seleccionar una.</p>
+          {isDragActive ? (
+            <p>Suelta la imagen aquí...</p>
+          ) : (
+            <p>Haz clic o arrastra una imagen aquí.</p>
+          )}
         </div>
 
         {uploadData && (
-          <code>
-            <pre>{JSON.stringify(uploadData, null, 2)}</pre>
-          </code>
+          <div>
+            <h2>Imagen cargada:</h2>
+            <img
+              src={uploadData.secure_url}
+              alt="Imagen cargada"
+              style={{ maxWidth: '100%', maxHeight: '400px' }}
+            />
+          </div>
         )}
       </main>
     </div>
