c26d28614059cd4715611a9f80cccd6f3d84a777
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-10-29 20:03:18 -0300

    Pagina Restringida Prueba

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -26,6 +26,7 @@ export default function Navbar() {
           />
         </Link>
         <Link href="/aboutUs">About Us</Link>
+        <Link href="/restringida">Retringida</Link>
       </div>
       {session ? (
         <div className="bg-gradient-to-b from-cyan-50 to-cyan-200 p-2 flex gap-5">
