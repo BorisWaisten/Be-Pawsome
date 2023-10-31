9b2ca0b28c0831ca60203e54dda7fb967f11586d
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-10-31 19:56:43 -0300

    SignIn Operacional

--- frontend/app/signIn/page.jsx
+++ frontend/app/signIn/page.jsx
@@ -8,16 +8,16 @@ import { signIn } from "next-auth/react";
 
 const login = async (datos) => {
   try {
-    const response = await axios.post('http://localhost:5000/usuarios/login', datos);
+    const response = await axios.post(
+      "http://localhost:5000/usuarios/login",
+      datos
+    );
     return response.data;
   } catch (error) {
     console.error(error);
     throw error; // Captura y lanza el error para que sea manejado por el componente
   }
-}
-
-
-
+};
 
 const FormLogin = () => {
   const router = useRouter();
@@ -27,8 +27,7 @@ const FormLogin = () => {
     password: "",
   });
 
- 
-const mandarANextAuth = async () => {
+  const mandarANextAuth = async () => {
     const result = await signIn("credentials", {
       mail: formData.mail,
       password: formData.password,
@@ -36,38 +35,19 @@ const mandarANextAuth = async () => {
       callbackUrl: "/",
     });
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
-        const response = await login(formData);
-        console.log(response.userLogueado._id);
-        console.log(response.accesToken);
-
-        const storedUser = response.userLogueado;
-        const idUsuario = storedUser ? storedUser._id : null;
-        const accessToken = response.accesToken; // Corregir la propiedad del token
-
-        if (idUsuario && accessToken) {
-          // Resto del código para obtener el usuario
-          // Agregar el ID del usuario y el token a sessionStorage
-          sessionStorage.setItem("idUsuario", idUsuario);
-          sessionStorage.setItem("token", accessToken);
-          mandarANextAuth();
-        } else {
-          console.error("No se pudo obtener el ID del usuario o el token.");
-        }
-       
-        // Almacena el usuario en sessionStorage
-        sessionStorage.setItem("user", JSON.stringify(storedUser));
-
-        // Llama a la función onLogin pasando el usuario
-        setApiError(null); // Limpia cualquier error existente
-        // Redirige al usuario a la página principal y forza la recarga del navbar
-        router.push("/"); //.then(() => window.location.reload());
-        router.refresh(); //este no logra mandar el refresh a la página principal
-      
+      const response = await login(formData);
+      const usuarioValido = response.userLogueado;
+      if (usuarioValido) {
+        mandarANextAuth();
+      } else {
+        console.error("No se pudo obtener el ID del usuario o el token.");
+      }
+      setApiError(null); // Limpia cualquier error existente
     } catch (error) {
       if (axios.isAxiosError(error)) {
         if (error.response.status === 401) {
@@ -78,13 +58,13 @@ const mandarANextAuth = async () => {
           );
         }
       } else {
-        setApiError("Ocurrió un error. Por favor, inténtalo de nuevo más tarde.");
+        setApiError(
+          "Ocurrió un error. Por favor, inténtalo de nuevo más tarde."
+        );
       }
     }
   };
 
-  //className="flex flex-col items-center"
-  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -134,8 +114,10 @@ const mandarANextAuth = async () => {
             Iniciar Sesión
           </button>
           <br />
-          <Link href="/signIn/recuperarContrasenia" 
-            className="bg-blue-500 hover:bg-violet-700 text-center text-white justify-center font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          <Link
+            href="/signIn/recuperarContrasenia"
+            className="bg-blue-500 hover:bg-violet-700 text-center text-white justify-center font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
             ¿Olvidaste tu contraseña?
           </Link>
         </form>
