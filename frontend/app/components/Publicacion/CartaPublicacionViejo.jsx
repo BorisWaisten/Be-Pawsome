c30ba24b574d1ea2453898ba92fe7ef877839945
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-10-31 17:00:13 -0300

    CartaPublicacion Actualizado

--- frontend/app/components/Publicacion/CartaPublicacionViejo.jsx
+++ frontend/app/components/Publicacion/CartaPublicacionViejo.jsx
@@ -7,11 +7,15 @@ import { useRouter } from "next/navigation"
 import { useSession } from "next-auth/react";
 import axios from "axios";
 
+
+
+
 function PublicacionCard({ publicacion }) {
   const { data: session } = useSession();
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [mostrarDetalles, setMostrarDetalles] = useState(false);
   const router = useRouter();
+
 
   function handlePrevImage() {
     setCurrentImageIndex((prevIndex) =>
@@ -24,31 +28,31 @@ function PublicacionCard({ publicacion }) {
       prevIndex === publicacion.animal.fotos.length - 1 ? 0 : prevIndex + 1
     );
   }
-
   const handleMostrarDetalles = () => {
     setMostrarDetalles(!mostrarDetalles);
   };
-
+  
   async function agregarACasita() {
     try {
-      const userId = session?.user?.userLogueado._id;
-      if (!userId) {
+        const userId = session?.user?.userLogueado._id;
+        if(!userId){
         router.push("/signIn");
       }
       const datos = {
         idAdoptante: userId,
         publicacion: publicacion,
       }
-      const mensajeSolicitud = await axios.post("http://localhost:5000/publicacion/solicitar", datos);
-      return mensajeSolicitud;
-    } catch (error) {
+      const mensajeSolicitud = await axios.post("http://localhost:5000/publicacion/solicitar", datos) 
+      return mensajeSolicitud
+    } catch (error){
       console.log(error);
     }
+    
   }
 
   return (
     <>
-      <div className="bg-white rounded-lg shadow-lg p-6 my-5 w-full h-full">
+      <div className="bg-white rounded-lg shadow-lg p-6 my-5 w-full h-full ">
         <h3 className="text-xl font-bold mb-4">{publicacion.titulo}</h3>
         <div className="slider-container relative">
           {/* Flecha izquierda */}
@@ -59,9 +63,10 @@ function PublicacionCard({ publicacion }) {
             &#10094;
           </span>
           {/* Imagen actual */}
+          
           <img
             className="rounded-lg w-full h-72 object-cover "
-            src={publicacion && publicacion.animal && publicacion.animal.fotos ? publicacion.animal.fotos[currentImageIndex] : ""}
+            src={publicacion.animal.fotos[currentImageIndex]}
             alt={`Foto ${currentImageIndex + 1}`}
           />
           {/* Flecha derecha */}
@@ -73,59 +78,59 @@ function PublicacionCard({ publicacion }) {
           </span>
         </div>
         {/* Detalles del animal */}
-        <div className="mb-4 bg-white rounded-lg shadow-lg p-4">
-          <p className="font-bold mb-2 text-lg">
-            Nombre: {publicacion.animal.nombre}
-          </p>
-          {mostrarDetalles ? (
-            <>
-              <p className="mb-2 text-m">Edad: {publicacion.animal.edad} años</p>
-              <p className="mb-2">Peso: {publicacion.animal.pesoEnKg} kg</p>
-              {/* Agrega más detalles según la estructura de tus datos */}
-              <p className="mb-2">
-                Descripcion: {publicacion.animal.descripcion}{' '}
-              </p>
-              <p className="mb-2">
-                Ubicacion: {publicacion.animal.ubicacion}
-              </p>
-              <p className="mb-2">
-                Historia Clinica: {publicacion.animal.historiaClinica}
-              </p>
-              <p className="font-bold text-lg">
-                Usuario Oferente: {publicacion.usuario.nombre}
-              </p>
+      <div className="mb-4 bg-white rounded-lg shadow-lg p-4">
+        <p className="font-bold mb-2 text-lg">
+          Nombre: {publicacion.animal.nombre}
+        </p>
+        {mostrarDetalles ? (
+          <>
+            <p className="mb-2 text-m">Edad: {publicacion.animal.edad} años</p>
+            <p className="mb-2">Peso: {publicacion.animal.pesoEnKg} kg</p>
+            {/* Agrega más detalles según la estructura de tus datos */}
+            <p className="mb-2">
+              Descripcion: {publicacion.animal.descripcion}{' '}
+            </p>
+            <p className="mb-2">
+              Ubicacion: {publicacion.animal.ubicacion}
+            </p>
+            <p className="mb-2">
+              Historia Clinica: {publicacion.animal.historiaClinica}
+            </p>
+            <p className="font-bold text-lg">
+              Usuario Oferente: {publicacion.usuario.nombre}
+            </p>
+            <button
+              className="text-blue-500 hover:underline hover:text-violet-500"
+              onClick={handleMostrarDetalles}
+            >
+              Ver menos
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="mb-2 text-m">
+              Edad: {publicacion.animal.edad} años{' '}
               <button
                 className="text-blue-500 hover:underline hover:text-violet-500"
                 onClick={handleMostrarDetalles}
               >
-                Ver menos
+                Ver más
               </button>
-            </>
-          ) : (
-            <>
-              <p className="mb-2 text-m">
-                Edad: {publicacion.animal.edad} años{' '}
-                <button
-                  className="text-blue-500 hover:underline hover:text-violet-500"
-                  onClick={handleMostrarDetalles}
-                >
-                  Ver más
-                </button>
-              </p>
-            </>
-          )}
-        </div>
-        <div>
-          <button
-            className="bg-blue-500 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded mx-auto mt-auto"
-            onClick={agregarACasita}
-          >
-            Me interesa
-          </button>
-        </div>
+            </p>
+          </>
+        )}
       </div>
+      <div>
+        <button
+          className="bg-blue-500 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded mx-auto mt-auto"
+          onClick={agregarACasita}
+        >
+          Me interesa
+        </button>
+      </div>
+    </div>
     </>
   );
 }
 
-export default PublicacionCard;
+export default PublicacionCard
