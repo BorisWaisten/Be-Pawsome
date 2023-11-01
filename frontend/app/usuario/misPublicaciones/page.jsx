652858c87a5a0645fc0a1705d37378b1edc6abdb
agustinc08 <loned@live.com.ar>
2023-11-01 20:27:13 -0300

    Cambio page

--- frontend/app/usuario/misPublicaciones/page.jsx
+++ frontend/app/usuario/misPublicaciones/page.jsx
@@ -16,7 +16,7 @@ const Page = () => {
 
   const cargarUsuario = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/publicacion/publicacione  sUsuario/${idUsuario}`);
+      const response = await axios.get(`http://localhost:5000/publicacion/publicacionesUsuario/${idUsuario}`);
       console.log(response.data);
       setPublicaciones(response.data);
       setUsuario(session?.user?.userLogueado);
