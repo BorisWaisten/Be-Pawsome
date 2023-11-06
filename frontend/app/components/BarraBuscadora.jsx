e8b7941d8ccf72d879a3d10dd1531be167c9e998
BorisWaisten <boriswaisten@gmail.com>
2023-11-06 20:38:59 -0300

    Arreglo en renderizacion

--- frontend/app/components/BarraBuscadora.jsx
+++ frontend/app/components/BarraBuscadora.jsx
@@ -4,27 +4,35 @@ import axios from "axios";
 import { useState } from "react"
 
 
-export default function BarraBuscadora({getPublicaciones}) {
+export default function BarraBuscadora({ handleSearch }) {
     const [query, setQuery] = useState("");
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-
-        const response = await axios.get(`http://localhost:5000/publicacion/buscar/${query}`);
-    
-        getPublicaciones(response.data);
-    };
+        handleSearch(query);
+      };
 
     return (
-        <div className="text-center my-20">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input className="text-black border-2 border-black rounded-full px-3 py-2" type="text" placeholder="Buscar Animal" value={query} onChange={(e) => setQuery(e.target.value)} />
-                </div>
-                <div>
-                    <button className="bg-black text-white rounded-full px-3 py-2 hover:bg-black/60" type="submit">Buscar</button>
-                </div>
-            </form>
+        <div className="flex items-center justify-center my-20">
+          <form onSubmit={handleSubmit} className="flex">
+            <div className="mr-2">
+              <input
+                className="text-black border-2 border-black rounded-full px-3 py-2"
+                type="text"
+                placeholder="Buscar Animal"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+            <div>
+              <button
+                className="bg-black text-white rounded-full px-3 py-3 hover:bg-black/60"
+                type="submit"
+              >
+                Buscar
+              </button>
+            </div>
+          </form>
         </div>
-    )
+      );
 }
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -5,47 +5,68 @@ import BarraBuscadora from "./components/BarraBuscadora.jsx"
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import PublicacionesList from "./components/Publicacion/PublicacionesList.jsx"
+import { set } from "mongoose";
 
 
 export default function page() {
   const { data: session } = useSession();
+  const [publicacionesOriginales, setPublicacionesOriginales] = useState([]); // Nuevo estado
   const [publicaciones, setPublicaciones] = useState([]);
   const [datosCargados, setDatosCargados] = useState(false);
+  const [query, setQuery] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:5000/publicacion/publicaciones");
         const publicacionesData = await response.json();
-  
+
         const idUsuario = session?.user?.userLogueado?._id;
-  
+
         if (idUsuario) {
           const publicacionesFiltradas = publicacionesData.filter((p) => p.usuario._id !== idUsuario);
           setPublicaciones(publicacionesFiltradas);
+          setPublicacionesOriginales(publicacionesFiltradas); // Almacena las originales
         } else {
           setPublicaciones(publicacionesData);
+          setPublicacionesOriginales(publicacionesData); // Almacena las originales
         }
-  
+
         setDatosCargados(true);
       } catch (error) {
         console.error("Error al obtener datos:", error);
       }
     };
-  
+
     fetchData();
   }, [session]);
-  
-  const getPublicaciones = (publicacionesSearch) => {
-    
-    const idUsuario = session?.user?.userLogueado?._id;
-    if (idUsuario) {
-      const publicacionesFiltradas = publicacionesSearch.filter((p) => p.usuario._id !== idUsuario);
-      setPublicaciones(publicacionesFiltradas);
-    }else{
-      setPublicaciones(publicacionesSearch);
+
+  const handleSearch = async (query) => {
+    try {
+      setQuery(query);
+      const response = await fetch(`http://localhost:5000/publicacion/buscar/${query}`);
+      const publicacionesBuscadas = await response.json();
+
+      const idUsuario = session?.user?.userLogueado?._id;
+
+      if (idUsuario) {
+        const publicacionesFiltradas = publicacionesBuscadas.filter((p) => p.usuario._id !== idUsuario);
+        setPublicaciones(publicacionesFiltradas);
+      } else {
+        setPublicaciones(publicacionesBuscadas);
+      }
+    } catch (error) {
+      console.error("Error al obtener datos de búsqueda:", error);
     }
-  }
+  };
+
+  // Restaurar las publicaciones originales al cambiar de URL
+  useEffect(() => {
+    if (!query) {
+      setPublicaciones(publicacionesOriginales);
+    }
+  }, [query, publicacionesOriginales]);
+
 
   if (!datosCargados) {
     // Mostrar un mensaje de carga o un spinner mientras se cargan los datos
@@ -60,7 +81,7 @@ export default function page() {
       <>
         <div>
           <Suspense fallback={<Cargando/>}>
-            <BarraBuscadora getPublicaciones={getPublicaciones}/>
+          <BarraBuscadora handleSearch={handleSearch} />
             <PublicacionesList publicaciones={publicaciones}/>
           </Suspense> 
         </div>
