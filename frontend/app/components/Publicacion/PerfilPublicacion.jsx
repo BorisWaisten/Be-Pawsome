f6d775f59011fff0db7fa76e3db310afd96592e4
Julian <julianstravitz@gmail.com>
2023-11-08 23:19:46 -0300

    cambio perfil publicacion

--- frontend/app/components/Publicacion/PerfilPublicacion.jsx
+++ frontend/app/components/Publicacion/PerfilPublicacion.jsx
@@ -42,88 +42,59 @@ function PerfilPublicacion({ publicacion }) {
   }
 
   return (
-    <>
-      <div className="bg-white rounded-lg shadow-lg p-6 my-5 w-full h-full flex flex-col md:flex-row">
-        {/* Columna de la imagen */}
-        <div className="slider-container relative flex-grow">
-          {/* Flecha izquierda */}
-          <span
-            className="slider-arrow left absolute top-1/2 transform -translate-y-1/2 cursor-pointer"
-            onClick={handlePrevImage}
-          >
-            &#10094;
-          </span>
-          {/* Imagen actual */}
-          <img
-            className="rounded-lg w-full h-72 object-cover "
-            style={{ height: 200, width: 300 }}
-            src={
-              publicacion && publicacion.animal && publicacion.animal.fotos
-                ? publicacion.animal.fotos[currentImageIndex]
-                : ""
-            }
-            alt={`Foto ${currentImageIndex + 1}`}
-          />
-          {/* Flecha derecha */}
-          <span
-            className="slider-arrow right absolute top-1/2 right-0 transform -translate-y-1/2 cursor-pointer"
-            onClick={handleNextImage}
-          >
-            &#10095;
-          </span>
-        </div>
-        {/* Columna de detalles */}
-        <div className="mb-4 bg-white rounded-lg shadow-lg p-4 flex-grow">
-          <h3 className="text-xl font-bold mb-4">{publicacion.titulo}</h3>
-          <button
-            className="bg-blue-500 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded mx-auto mt-auto mb-4"
-            onClick={agregarACasita}
-          >
-            Quiero adoptarlo
-          </button>
-          <p className="font-bold mb-2 text-lg">
-            Nombre: {publicacion.animal.nombre}
-          </p>
-          {mostrarDetalles ? (
-            <>
-              <p className="mb-2 text-m">
-                Edad: {publicacion.animal.edad} años
-              </p>
-              <p className="mb-2">Peso: {publicacion.animal.pesoEnKg} kg</p>
-              {/* Agrega más detalles según la estructura de tus datos */}
-              <p className="mb-2">
-                Descripcion: {publicacion.animal.descripcion}{" "}
-              </p>
-              <p className="mb-2">Ubicacion: {publicacion.animal.ubicacion}</p>
-              <p className="mb-2">
-                Historia Clinica: {publicacion.animal.historiaClinica}
-              </p>
-              <p className="font-bold text-lg">
-                Usuario Oferente: {publicacion.usuario.nombre}
-              </p>
-              <button
-                className="text-blue-500 hover:underline hover:text-violet-500"
-                onClick={handleMostrarDetalles}
-              >
-                Ver menos
-              </button>
-            </>
-          ) : (
-            <>
-              <p className="mb-2 text-m">
-                Edad: {publicacion.animal.edad} años{" "}
-                <button
-                  className="text-blue-500 hover:underline hover:text-violet-500"
-                  onClick={handleMostrarDetalles}
-                >
-                  Ver más
-                </button>
-              </p>
-            </>
-          )}
-        </div>
+    <div className="flex flex-col md:flex-row bg-white rounded-lg shadow-lg p-6 my-5 w-full h-full">
+      {/* Columna de la imagen */}
+      <div className="slider-container relative w-full md:w-1/2">
+        {/* Flecha izquierda */}
+        <span
+          className="slider-arrow left absolute top-1/2 transform -translate-y-1/2 cursor-pointer"
+          onClick={handlePrevImage}
+        >
+          &#10094;
+        </span>
+        {/* Imagen actual */}
+        <img
+          className="rounded-lg w-full h-auto object-cover"
+          src={
+            publicacion && publicacion.animal && publicacion.animal.fotos
+              ? publicacion.animal.fotos[currentImageIndex]
+              : ""
+          }
+          alt={`Foto ${currentImageIndex + 1}`}
+        />
+        {/* Flecha derecha */}
+        <span
+          className="slider-arrow right absolute top-1/2 right-0 transform -translate-y-1/2 cursor-pointer"
+          onClick={handleNextImage}
+        >
+          &#10095;
+        </span>
       </div>
-    </>
+      {/* Columna de detalles */}
+      <div className="mb-4 bg-white rounded-lg  p-4 w-full md:w-1/2">
+        <h3 className="text-xl font-bold mb-4">{publicacion.titulo}</h3>
+        <button
+          className="bg-blue-500 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded mx-auto mt-auto mb-4"
+          onClick={agregarACasita}
+        >
+          Quiero adoptarlo
+        </button>
+        <p className="font-bold mb-2 text-lg">
+          Nombre: {publicacion.animal.nombre}
+        </p>
+        <p className="mb-2 text-m">Edad: {publicacion.animal.edad} años</p>
+        <p className="mb-2">Peso: {publicacion.animal.pesoEnKg} kg</p>
+        {/* Agrega más detalles según la estructura de tus datos */}
+        <p className="mb-2">Descripcion: {publicacion.animal.descripcion} </p>
+        <p className="mb-2">Ubicacion: {publicacion.animal.ubicacion}</p>
+        <p className="mb-2">
+          Historia Clinica: {publicacion.animal.historiaClinica}
+        </p>
+        <p className="font-bold text-lg">
+          Usuario Oferente: {publicacion.usuario.nombre}
+        </p>
+      </div>
+    </div>
   );
 }
 export default PerfilPublicacion;
