a559911e6b36b453d5f501246896b308266205cd
Julian <julianstravitz@gmail.com>
2023-11-18 00:12:38 -0300

    css footer

--- frontend/app/components/Layout/Footer.jsx
+++ frontend/app/components/Layout/Footer.jsx
@@ -1,14 +1,14 @@
 export default function Footer() {
  
   return (
-    <div id="seccion_footer" className="sticky mt-96 scroll-mt-10 top-[100vh] w-full bg-neutral-200 lg:text-left">
+    <div id="seccion_footer" className="sticky mt-96 scroll-mt-10 top-[100vh] w-full bg-purple-100 lg:text-left">
       <div className="p-4 text-center text-neutral-800 dark:text-neutral-300">
         <p>© 2023 Copyright: GALAJO ¿Qué puedo hacer en caso de maltrato o crueldad con animales?</p>
         <a
-          className="text-neutral-900 dark:text-neutral-400"
+          className="text-purple-400 dark:text-purple-400"
           href="https://www.argentina.gob.ar/normativa/nacional/ley-14346-153011/texto"
         >
-          <p className="font-bold m-2">Ir a Proteccion de los animales </p>
+          <p className="font-bold m-2 text-purple-500">Ir a Proteccion de los animales </p>
         </a>
       
       </div>
