5c8cc0eee3fb8af051f489f7c26290ccdb4c6ca1
Julian <julianstravitz@gmail.com>
2023-11-17 23:33:12 -0300

    css iniciar sesion y crear publicacion

--- frontend/app/signIn/page.jsx
+++ frontend/app/signIn/page.jsx
@@ -38,12 +38,17 @@ const FormLogin = () => {
 
   const handleError = (error) => {
     if (axios.isAxiosError(error)) {
-      if(error.response.data === "\"password\" failed custom validation because La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un número"){
-        setApiError("La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un número");
-      }else{
-        setApiError(error.response.data); 
+      if (
+        error.response.data ===
+        '"password" failed custom validation because La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un número'
+      ) {
+        setApiError(
+          "La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un número"
+        );
+      } else {
+        setApiError(error.response.data);
       }
-    } 
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -73,58 +78,66 @@ const FormLogin = () => {
 
   return (
     <>
-      <h2 className="text-2xl font-bold mb-4">Loguearse</h2>
-      <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <label htmlFor="mail" className="block text-gray-700 font-bold mb-2">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="mail"
-            name="mail"
-            placeholder="Email"
-            onChange={handleChange}
-            onFocus={() => setApiError(null)}
-            required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
-          />
-          <label
-            htmlFor="password"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Contraseña:
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Contraseña"
-            onChange={handleChange}
-            onFocus={() => setApiError(null)}
-            required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-violet-700 text-center text-white justify-center font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Iniciar Sesión
-          </button>
-          <br />
-          <Link
-            href="/signIn/recuperarContrasenia"
-            className="bg-blue-500 hover:bg-violet-700 text-center text-white justify-center font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            ¿Olvidaste tu contraseña?
-          </Link>
-        </form>
-      </div>
-      {apiError && (
-        <div className="error card my-5">
-          <p>{apiError}</p>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-1/2">
+          <h2 className="text-2xl font-bold mb-4 justify-center flex">Iniciar Sesion</h2>
+          <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+            <form onSubmit={handleSubmit} className="flex flex-col">
+              <label
+                htmlFor="mail"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                id="mail"
+                name="mail"
+                placeholder="Email"
+                onChange={handleChange}
+                onFocus={() => setApiError(null)}
+                required
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+              />
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Contraseña"
+                onChange={handleChange}
+                onFocus={() => setApiError(null)}
+                required
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+              />
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-violet-700 text-center text-white justify-center font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Iniciar Sesión
+              </button>
+              <br />
+              <Link
+                href="/signIn/recuperarContrasenia"
+                className="bg-blue-500 hover:bg-violet-700 text-center text-white justify-center font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                ¿Olvidaste tu contraseña?
+              </Link>
+            </form>
+          </div>
+          {apiError && (
+            <div className="error card my-5">
+              <p>{apiError}</p>
+            </div>
+          )}
         </div>
-      )}
+      </div>
+      )
     </>
   );
 };
--- frontend/app/uploadImagen/animal/page.jsx
+++ frontend/app/uploadImagen/animal/page.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 function ImagenAnimal({ onImageUpload }) {
   const [uploadData, setUploadData] = useState([]);
@@ -10,16 +10,19 @@ function ImagenAnimal({ onImageUpload }) {
       // Itera sobre cada archivo aceptado
       for (const file of acceptedFiles) {
         const dataImagen = new FormData();
-        dataImagen.append('file', file);
-        dataImagen.append('upload_preset', 'animales');
+        dataImagen.append("file", file);
+        dataImagen.append("upload_preset", "animales");
 
-        const response = await fetch('https://api.cloudinary.com/v1_1/bepawsome/image/upload', {
-          method: 'POST',
-          body: dataImagen,
-        });
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/bepawsome/image/upload",
+          {
+            method: "POST",
+            body: dataImagen,
+          }
+        );
 
         if (!response.ok) {
-          throw new Error('Error en la carga de la imagen');
+          throw new Error("Error en la carga de la imagen");
         }
 
         const data = await response.json();
@@ -31,31 +34,39 @@ function ImagenAnimal({ onImageUpload }) {
         onImageUpload(data.secure_url);
       }
     } catch (error) {
-      console.error('Error en la carga de la imagen:', error);
-      setError('Error en la carga de la imagen. Por favor, inténtalo de nuevo.');
+      console.error("Error en la carga de la imagen:", error);
+      setError(
+        "Error en la carga de la imagen. Por favor, inténtalo de nuevo."
+      );
     }
+  };
+
+  const handleRemove = (index) => {
+    const newUploadData = [...uploadData];
+    newUploadData.splice(index, 1);
+    setUploadData(newUploadData);
   };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
-    accept: 'image/*',
+    accept: "image/*",
   });
 
   return (
     <div>
       <main>
-        <h1>Subir Imagen Animal</h1>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <h3>Subir Imagen Animal</h3>
+        {error && <p style={{ color: "red" }}>{error}</p>}
 
         <div
           {...getRootProps()}
           style={{
-            border: '2px dashed #777',
-            borderRadius: '8px',
-            padding: '20px',
-            textAlign: 'center',
-            cursor: 'pointer',
-            backgroundColor: isDragActive ? '#eee' : '#fff',
+            border: "2px dashed #777",
+            borderRadius: "8px",
+            padding: "20px",
+            textAlign: "center",
+            cursor: "pointer",
+            backgroundColor: isDragActive ? "#eee" : "#fff",
           }}
         >
           <input {...getInputProps()} />
@@ -65,22 +76,29 @@ function ImagenAnimal({ onImageUpload }) {
             <p>Haz clic o arrastra una imagen aquí.</p>
           )}
         </div>
-        
+
         {uploadData.length > 0 && (
           <div>
-            <h2>Imágenes cargadas:</h2>
-            {uploadData.map((data, index) => (
-              <div key={index}>
-                <img
-                  src={data.secure_url}
-                  alt={`Imagen cargada ${index + 1}`}
-                  style={{ maxWidth: '100%', maxHeight: '400px' }}
-                />
-              </div>
-            ))}
+            <h3>Imágenes cargadas:</h3>
+            <div className="grid grid-cols-3 gap-4">
+              {uploadData.map((data, index) => (
+                <div key={index} className="relative">
+                  <img
+                    src={data.secure_url}
+                    alt={`Imagen cargada ${index + 1}`}
+                    className="object-cover p-1 h-64 w-full"
+                  />
+                  <button
+                    onClick={() => handleRemove(index)}
+                    className="absolute right-2 top-3 rounded-xl bg-purple-300 text-white p-1"
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         )}
-        
       </main>
     </div>
   );
