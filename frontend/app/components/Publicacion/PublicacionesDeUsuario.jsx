2ec38c30a7bcbf2a07fd2d3744320b10e9960ac2
Julian <julianstravitz@gmail.com>
2023-11-20 17:40:37 -0300

    Revert "agregado boton de interesados para concretar adopcion"
    This reverts commit e2c5624f80e205b11ab3e2fc23fbb3e5f1cc2962.

--- frontend/app/components/Publicacion/PublicacionesDeUsuario.jsx
+++ frontend/app/components/Publicacion/PublicacionesDeUsuario.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 
 const PublicacionesDeUsuario = ({ publicaciones }) => {
   const [confirmacionEliminar, setConfirmacionEliminar] = useState(null);
-  const [desplegarInteresados, setDesplegarInteresados] = useState({});
-  
 
   const confirmarEliminar = (publicacionId) => {
     setConfirmacionEliminar(publicacionId);
@@ -12,9 +10,7 @@ const PublicacionesDeUsuario = ({ publicaciones }) => {
 
   const eliminarPublicacion = async (publicacionId) => {
     try {
-      await axios.delete(
-        `http://localhost:5000/publicacion/eliminar/${publicacionId}`
-      );
+      await axios.delete(`http://localhost:5000/publicacion/eliminar/${publicacionId}`);
       window.location.reload();
     } catch (error) {
       console.error(error);
@@ -26,51 +22,35 @@ const PublicacionesDeUsuario = ({ publicaciones }) => {
 
   return (
     <div>
-      <h1 className="mt-3 items-center justify-center flex">
-        Mis publicaciones
-      </h1>
+      <h1 className="mt-3 items-center justify-center flex">Mis publicaciones</h1>
       <ul className="divide-y divide-violet-200">
         {Array.isArray(publicaciones) &&
           publicaciones.map((publicacion) => (
             <li key={publicacion._id} className="my-4">
-              {/* ...otros códigos... */}
-              <button
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => setDesplegarInteresados(!desplegarInteresados)}
-              >
-                Ver interesados
-              </button>
-              {desplegarInteresados && (
-                <ul className="mt-2 bg-white shadow rounded p-4">
-                  {publicacion.interesados.map((interesado) => (
-                    <li
-                      key={interesado._id}
-                      className="flex justify-between items-center mb-2"
-                    >
-                      <span className="text-gray-700">{interesado.nombre}</span>
-                      <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-                        onClick={() => concretarAdopcion(interesado._id)}
-                      >
-                        Concretar adopción
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              )}
+              <div className="flex items-center justify-between space-x-5 border-1 border-gray-200 py-4 px-2">
+                <div className="rounded-lg w-24 h-24 overflow-hidden">
+                  <img src={publicacion.animal.fotos[0]} className="object-cover w-full h-full" />
+                </div>
+                <div className="flex-1">
+                  <div className="text-lg font-medium text-violet-900 truncate">{publicacion.titulo}</div>
+                  <div className="text-sm text-black-500">{publicacion.animal.descripcion}</div>
+                </div>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => confirmarEliminar(publicacion._id)}
+                >
+                  Eliminar
+                </button>
+              </div>
             </li>
           ))}
       </ul>
-
-      
 
       {/* Confirmación de eliminación */}
       {confirmacionEliminar && (
         <div>
           <p>¿Estás seguro de que deseas eliminar esta publicación?</p>
-          <button onClick={() => eliminarPublicacion(confirmacionEliminar)}>
-            Sí
-          </button>
+          <button onClick={() => eliminarPublicacion(confirmacionEliminar)}>Sí</button>
           <button onClick={() => setConfirmacionEliminar(null)}>No</button>
         </div>
       )}
