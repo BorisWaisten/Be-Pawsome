157eb4b2a720de6f7f60a857779749fc6eba3bfe
Julian <julianstravitz@gmail.com>
2023-11-21 13:11:13 -0300

    arreglos de css en eliminar publicacion

--- frontend/app/components/Publicacion/PublicacionesDeUsuario.jsx
+++ frontend/app/components/Publicacion/PublicacionesDeUsuario.jsx
@@ -10,7 +10,9 @@ const PublicacionesDeUsuario = ({ publicaciones }) => {
 
   const eliminarPublicacion = async (publicacionId) => {
     try {
-      await axios.delete(`http://localhost:5000/publicacion/eliminar/${publicacionId}`);
+      await axios.delete(
+        `http://localhost:5000/publicacion/eliminar/${publicacionId}`
+      );
       window.location.reload();
     } catch (error) {
       console.error(error);
@@ -22,38 +24,63 @@ const PublicacionesDeUsuario = ({ publicaciones }) => {
 
   return (
     <div>
-      <h1 className="mt-3 items-center justify-center flex">Mis publicaciones</h1>
+      <h1 className="mt-3 items-center justify-center flex">
+        Mis publicaciones
+      </h1>
       <ul className="divide-y divide-violet-200">
         {Array.isArray(publicaciones) &&
           publicaciones.map((publicacion) => (
             <li key={publicacion._id} className="my-4">
               <div className="flex items-center justify-between space-x-5 border-1 border-gray-200 py-4 px-2">
                 <div className="rounded-lg w-24 h-24 overflow-hidden">
-                  <img src={publicacion.animal.fotos[0]} className="object-cover w-full h-full" />
+                  <img
+                    src={publicacion.animal.fotos[0]}
+                    className="object-cover w-full h-full"
+                  />
                 </div>
                 <div className="flex-1">
-                  <div className="text-lg font-medium text-violet-900 truncate">{publicacion.titulo}</div>
-                  <div className="text-sm text-black-500">{publicacion.animal.descripcion}</div>
+                  <div className="text-lg font-medium text-violet-900 truncate">
+                    {publicacion.titulo}
+                  </div>
+                  <div className="text-sm text-black-500">
+                    {publicacion.animal.descripcion}
+                  </div>
                 </div>
                 <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => confirmarEliminar(publicacion._id)}
+                >
+                  Ver interesados + (publicacion.id.interesados.length)
+                </button>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded relative"
                   onClick={() => confirmarEliminar(publicacion._id)}
                 >
                   Eliminar
                 </button>
+                  {/* Confirmación de eliminación */}
+                  {confirmacionEliminar === publicacion._id && (
+                    <div className=" p-4 rounded shadow-lg">
+                      <p className=" font-extrabold text-justify p-1 mb-2">
+                        ¿Estás seguro de que deseas eliminar esta publicación?
+                      </p>
+                      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-2"
+                        onClick={() =>
+                          eliminarPublicacion(confirmacionEliminar)
+                        }
+                      >
+                        Sí
+                      </button>
+                      <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2" onClick={() => setConfirmacionEliminar(null)}>
+                        No
+                      </button>
+                    </div>
+                  )}
+               
               </div>
             </li>
           ))}
       </ul>
-
-      {/* Confirmación de eliminación */}
-      {confirmacionEliminar && (
-        <div>
-          <p>¿Estás seguro de que deseas eliminar esta publicación?</p>
-          <button onClick={() => eliminarPublicacion(confirmacionEliminar)}>Sí</button>
-          <button onClick={() => setConfirmacionEliminar(null)}>No</button>
-        </div>
-      )}
     </div>
   );
 };
