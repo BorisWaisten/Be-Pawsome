97bc2d9ba590d62b81cec40f0868ecb4be7927ee
Julian <julianstravitz@gmail.com>
2023-11-21 16:05:28 -0300

    correccion filtro de estado de publicacion

--- frontend/app/components/Publicacion/PublicacionesList.jsx
+++ frontend/app/components/Publicacion/PublicacionesList.jsx
@@ -41,7 +41,7 @@ export default function PublicacionesList(publicacionesSearch) {
       )}
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10">
         {currentPosts.map((publicacion) =>
-          publicacion.estado === "ACTIVA" ? (
+          publicacion.estadoPublicacion === "ACTIVA" ? (
             <li key={publicacion._id} className="w-full h-full">
               <CartaPublicacion publicacion={publicacion} />
             </li>
