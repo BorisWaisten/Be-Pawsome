967dd613a4ea3eb9abea014a4759865c227df21f
agustinc08 <loned@live.com.ar>
2023-11-22 21:34:19 -0300

    asd

--- frontend/app/usuario/misPublicaciones/page.jsx
+++ frontend/app/usuario/misPublicaciones/page.jsx
@@ -4,14 +4,14 @@ import PublicacionesDeUsuario from "../../components/Publicacion/PublicacionesDe
 import axios from "axios";
 import { useSession } from "next-auth/react";
 
-const Page = (idUsuario) => {
+const Page = () => {
   const { data: session } = useSession();
   const [usuario, setUsuario] = useState(null);
   const [publicaciones, setPublicaciones] = useState([]);
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const idUsuario = session?.user?.userLogueado._id;
 
-
-  const cargarUsuario = async (idUsuario) => {
+  const cargarUsuario = async () => {
     try {
       const response = await axios.get(`${apiUrl}/publicacion/publicacionesUsuario/${idUsuario}`);
       console.log(response.data);
@@ -25,7 +25,7 @@ const Page = (idUsuario) => {
   useEffect(() => {
     cargarUsuario();
     console.log(session); // Imprime la información de la sesión
-    console.log(idUsuario); // Imprime la información del usuario
+    console.log(usuario); // Imprime la información del usuario
   }, [session]);
   
   return (
